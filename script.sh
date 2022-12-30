4993c017f2895187444a4c24085e53a8e93265d5
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2022-12-30 18:08:17 +0530

    file fix

--- script.sh
+++
@@ -1,7 +0,0 @@
-echo "# portfolio" >> README.md
-git init
-git add README.md
-git commit -m "git"
-git branch -M main
-git remote add origin https://github.com/ritik-prog/portfolio.git
-git push -u origin main
